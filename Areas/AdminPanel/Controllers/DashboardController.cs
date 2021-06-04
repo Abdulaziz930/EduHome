df406998d6753d04d9f7f70828530c80d2581d74
Abdulaziz930 <abdulazizak@code.edu.az>
2021-06-05 01:40:01 +0400

    admin panel created

--- Areas/AdminPanel/Controllers/DashboardController.cs
+++ Areas/AdminPanel/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EduHome.Areas.AdminPanel.Controllers
 {
+    [Area("AdminPanel")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
