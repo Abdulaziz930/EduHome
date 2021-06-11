144e79e1d19e9137d9c6ca254f551d76b8293ff0
Abdulaziz930 <abdulazizak@code.edu.az>
2021-06-11 14:13:29 +0400

    event's CRUD is done

--- Areas/AdminPanel/Controllers/EventController.cs
+++ Areas/AdminPanel/Controllers/EventController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using EduHome.Areas.AdminPanel.Utils;
@@ -83,5 +84,139 @@ namespace EduHome.Areas.AdminPanel.Controllers
         }
 
         #endregion
+
+        #region Update
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var @event = await _db.Events.Include(x => x.EventDetail)
+                .Where(x => x.EventDetail.IsDeleted == false)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (@event == null)
+                return NotFound();
+
+            return View(@event);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id,Event @event)
+        {
+            if (id == null)
+                return NotFound();
+
+            if (id != @event.Id)
+                return BadRequest();
+
+            var dbEvent = await _db.Events.Include(x => x.EventDetail)
+                .Where(x => x.EventDetail.IsDeleted == false)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (dbEvent == null)
+                return NotFound();
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var fileName = dbEvent.Image;
+
+            if (@event.Photo != null)
+            {
+                if (!@event.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "This is not a picture");
+                    return View();
+                }
+
+                if (!@event.Photo.IsSizeAllowed(3000))
+                {
+                    ModelState.AddModelError("Photo", "The size of the image you uploaded is 3 MB higher.");
+                    return View();
+                }
+
+                var path = Path.Combine(Constants.ImageFolderPath, "event", dbEvent.Image);
+
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+
+                fileName = await FileUtil.GenerateFileAsync(Constants.ImageFolderPath, "event", @event.Photo);
+            }
+
+            dbEvent.Image = fileName;
+            dbEvent.Title = @event.Title;
+            dbEvent.Venue = @event.Venue;
+            dbEvent.StartTime = @event.StartTime;
+            dbEvent.EndTime = @event.EndTime;
+            dbEvent.EventDetail = @event.EventDetail;
+            dbEvent.LastModificationDate = DateTime.Now;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var @event = await _db.Events.Include(x => x.EventDetail)
+                .Where(x => x.EventDetail.IsDeleted == false)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (@event == null)
+                return NotFound();
+
+            return View(@event);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteEvent(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var @event = await _db.Events.Include(x => x.EventDetail)
+                .Where(x => x.EventDetail.IsDeleted == false)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (@event == null)
+                return NotFound();
+
+            @event.IsDeleted = true;
+            @event.EventDetail.IsDeleted = true;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var @event = await _db.Events.Include(x => x.EventDetail)
+                .Include(x => x.EventSpeakers)
+                .ThenInclude(x => x.Speaker)
+                .Where(x => x.EventDetail.IsDeleted == false)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (@event == null)
+                return NotFound();
+
+            return View(@event);
+        }
     }
 }
