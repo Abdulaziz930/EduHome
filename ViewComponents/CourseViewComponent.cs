b8b594481aa4c4c7b52bc23973cf816d91133bad
Abdulaziz930 <abdulazizak@code.edu.az>
2021-06-14 00:53:59 +0400

    course section's front to back is done

---
+++ ViewComponents/CourseViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using EduHome.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.ViewComponents
+{
+    public class CourseViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _db;
+
+        public CourseViewComponent(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int count = 9)
+        {
+            var courses = await _db.Courses.Where(x => x.IsDeleted == false)
+                .OrderByDescending(x => x.LastModificationDate).Take(count).ToListAsync();
+
+            return View(courses);
+        }
+    }
+}
