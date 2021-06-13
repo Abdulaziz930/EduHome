7c1bad7c98a637cf6702a8036a14985b33627c64
Abdulaziz930 <abdulazizak@code.edu.az>
2021-06-14 00:08:06 +0400

    socialMedia's CRUD is done

--- Areas/AdminPanel/Controllers/SocialMediaController.cs
+++ Areas/AdminPanel/Controllers/SocialMediaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EduHome.DataAccessLayer;
 using EduHome.Models;
+using EduHome.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -24,7 +25,7 @@ namespace EduHome.Areas.AdminPanel.Controllers
             ViewBag.PageCount = Math.Ceiling((decimal)(_db.Teachers.Count() / 5));
             ViewBag.Page = page;
 
-            var teachers = await _db.Teachers.Where(x => x.IsDeleted == false)
+            var teachers = await _db.Teachers.Where(x => x.IsDeleted == false).Include(x => x.SocialMedias)
                 .OrderByDescending(x => x.Id).Skip((page - 1) * 5).Take(5).ToListAsync();
 
             return View(teachers);
@@ -50,7 +51,7 @@ namespace EduHome.Areas.AdminPanel.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(SocialMedia socialMedia,int teacherId)
         {
-            var teachers = await _db.Teachers.Where(x => x.IsDeleted == false).ToListAsync();
+            var teachers = await _db.Teachers.Where(x => x.IsDeleted == false).Include(x => x.SocialMedias).ToListAsync();
             ViewBag.Teachers = teachers;
             Dictionary<string, string> socialIcons = new Dictionary<string, string>();
             socialIcons.Add("Facebook", "zmdi zmdi-facebook");
@@ -64,6 +65,21 @@ namespace EduHome.Areas.AdminPanel.Controllers
                 return View();
             }
 
+            foreach (var teacher in teachers)
+            {
+                if(teacher.Id == teacherId)
+                {
+                    foreach (var item in teacher.SocialMedias)
+                    {
+                        if(item.Link == socialMedia.Link && item.Icon == socialMedia.Icon)
+                        {
+                            ModelState.AddModelError("", "is exists");
+                            return View();
+                        }
+                    }
+                }
+            }
+
             socialMedia.TeacherId = teacherId;
             socialMedia.IsDeleted = false;
 
@@ -74,5 +90,119 @@ namespace EduHome.Areas.AdminPanel.Controllers
         }
 
         #endregion
+
+        #region Update
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var socialMedia = await _db.SocialMedias.Include(x => x.Teacher)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (socialMedia == null)
+                return NotFound();
+
+            Dictionary<string, string> socialIcons = new Dictionary<string, string>();
+            socialIcons.Add("Facebook", "zmdi zmdi-facebook");
+            socialIcons.Add("Pinterest", "zmdi zmdi-pinterest");
+            socialIcons.Add("Vimeo", "zmdi zmdi-vimeo");
+            socialIcons.Add("Twitter", "zmdi zmdi-twitter");
+            ViewBag.Icons = socialIcons;
+
+            return View(socialMedia);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id,SocialMedia socialMedia)
+        {
+            if (id == null)
+                return NotFound();
+
+            if (id != socialMedia.Id)
+                return BadRequest();
+
+            var dbSocialMedia = await _db.SocialMedias.Include(x => x.Teacher)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (dbSocialMedia == null)
+                return NotFound();
+
+            Dictionary<string, string> socialIcons = new Dictionary<string, string>();
+            socialIcons.Add("Facebook", "zmdi zmdi-facebook");
+            socialIcons.Add("Pinterest", "zmdi zmdi-pinterest");
+            socialIcons.Add("Vimeo", "zmdi zmdi-vimeo");
+            socialIcons.Add("Twitter", "zmdi zmdi-twitter");
+            ViewBag.Icons = socialIcons;
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            dbSocialMedia.Link = socialMedia.Link;
+            dbSocialMedia.Icon = socialMedia.Icon;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Delete
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var socialMedia = await _db.SocialMedias.Include(x => x.Teacher)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (socialMedia == null)
+                return NotFound();
+
+            return View(socialMedia);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteSocialMedia(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var socialMedia = await _db.SocialMedias.Include(x => x.Teacher)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (socialMedia == null)
+                return NotFound();
+
+            socialMedia.IsDeleted = true;
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Detail
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var socialMedia = await _db.SocialMedias.Include(x => x.Teacher)
+                .FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == false);
+            if (socialMedia == null)
+                return NotFound();
+
+            return View(socialMedia);
+        }
+
+        #endregion
     }
+    
 }
---
+++ ViewModels/SocialMediaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EduHome.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.ViewModels
+{
+    public class SocialMediaViewModel
+    {
+        public List<Teacher> Teachers { get; set; }
+
+        public List<SocialMedia> SocialMedias { get; set; }
+    }
+}
