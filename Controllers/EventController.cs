a9e5538a68c86ff7d78065a36416f4925d650475
Abdulaziz930 <abdulazizak@code.edu.az>
2021-06-14 15:01:57 +0400

    event and event detail pages front to back is done

---
+++ Controllers/EventController.cs
@@ -0,0 +1,45 @@
+﻿using EduHome.DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHome.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public EventController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            ViewBag.PageCount = Decimal.Ceiling(_db.Courses.Count() / 9);
+            ViewBag.Page = page;
+
+            var events = await _db.Events.Where(x => x.IsDeleted == false)
+                .OrderByDescending(x => x.LastModificationDate).Skip((page - 1) * 9).Take(9).ToListAsync();
+
+            return View(events);
+        }
+
+        public async Task<IActionResult> EventDetail(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var @event = await _db.Events.Where(x => x.IsDeleted == false)
+                .Include(x => x.EventDetail).Include(x => x.EventSpeakers)
+                .ThenInclude(x => x.Speaker).FirstOrDefaultAsync(x => x.Id == id);
+            if (@event == null)
+                return NotFound();
+
+            return View(@event);
+        }
+    }
+}
