5c20728995344598ab1c89bc6d322093b5cb0b20
Abdulaziz930 <abdulazizak@code.edu.az>
2021-06-14 15:33:30 +0400

    course page's pagination is updated

--- ViewComponents/CourseViewComponent.cs
+++ ViewComponents/CourseViewComponent.cs
@@ -17,12 +17,19 @@ namespace EduHome.ViewComponents
             _db = db;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(int count = 9)
+        public async Task<IViewComponentResult> InvokeAsync(InvokeRequest invokeRequest)
         {
+            
+
             var courses = await _db.Courses.Where(x => x.IsDeleted == false)
-                .OrderByDescending(x => x.LastModificationDate).Take(count).ToListAsync();
+                .OrderByDescending(x => x.LastModificationDate).Skip((invokeRequest.SkipCount - 1) * invokeRequest.Count).Take(invokeRequest.Count).ToListAsync();
 
             return View(courses);
         }
+    }
+    public class InvokeRequest
+    {
+        public int Count { get; set; } = 9;
+        public int SkipCount { get; set; } = 0;
     }
 }
